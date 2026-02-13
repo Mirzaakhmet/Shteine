b3a30b48ca9fc0e9f016a3cb51751eca3a2f96f1
Mirzaakhmet <m_kulibay@kbtu.kz>
2026-02-13 13:00:43 +0500

    up

--- LAB3/Problem1/Analyzer.java
+++ LAB3/Problem1/Analyzer.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class Analyzer {
 	public static  void main(String[] args) {
-		Scanner s =  new Scanner(System.in);
-		Data dataSet = new Data();
-		while(true) {
-			System.out.println("Enter number (Q to quit): ");
-			if (s.hasNextDouble()) {
-				double in = s.nextDouble();
-				dataSet.add(in);
-			}
-			else {
-				String in = s.next();
-				if (in.equalsIgnoreCase("Q")) {
-					break;
-				}
-				else {
-					System.out.println("Invalid input. Please enter a number or 'Q'.");
-				}
-			}
-		}
-	
-		System.out.println("Average = " + dataSet.getAvg());
-        System.out.println("Maximum = " + dataSet.getMax());
-		s.close();
+            try (Scanner s = new Scanner(System.in)) {
+                Data dataSet = new Data();
+                while(true) {
+                    System.out.println("Enter number (Q to quit): ");
+                    if (s.hasNextDouble()) {
+                        double in = s.nextDouble();
+                        dataSet.add(in);
+                    }
+                    else {
+                        String in = s.next();
+                        if (in.equalsIgnoreCase("Q")) {
+                            break;
+                        }
+                        else {
+                            System.out.println("Invalid input. Please enter a number or 'Q'.");
+                        }
+                    }
+                }
+                
+                System.out.println("Average = " + dataSet.getAvg());
+                System.out.println("Maximum = " + dataSet.getMax());
+            }
 	}
 }
